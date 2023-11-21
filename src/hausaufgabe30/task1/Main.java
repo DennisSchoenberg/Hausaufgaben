ab2a84d3ddd90345533f131e70a88893b567c804
Dennis Schönberg <dennis_schoenberg@yahoo.de>
2023-11-21 19:57:03 +0100

    Homework30

--- src/hausaufgabe30/task1/Main.java
+++ src/hausaufgabe30/task1/Main.java
@@ -2,6 +2,25 @@ package hausaufgabe30.task1;
 
 public class Main {
     public static void main(String[] args) {
+        // Целочисленная переменная
+        int integerVar = 15;
+        System.out.println("Целочисленная: " + integerVar + ", " + Integer.BYTES + " байта");
+
+        // Нецелая переменная (тип double)
+        double doubleVar = 6.12;
+        System.out.println("Нецелая: " + doubleVar + ", " + Double.BYTES + " байт");
+
+        // Символьная переменная
+        char charVar = 'A';
+        System.out.println("Символьная: " + charVar + ", " + Character.BYTES + " байт");
+
+        // Логическая переменная
+        boolean booleanVar = true;
+        System.out.println("Логическая: " + booleanVar + ", 1 байт");
+
 
     }
 }
+
+
+
